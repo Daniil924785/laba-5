2bad223144ba3fe6aa5ca90aa101c2c901db2cec
Daniil924785 <54883329+Daniil924785@users.noreply.github.com>
2022-12-18 19:46:16 +0300

    Add files via upload

---
+++ Category.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using WpfApp1;
+
+namespace GetStartedWPF
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Product>
+            Products
+        { get; private set; } =
+            new ObservableCollection<Product>();
+    }
+}
